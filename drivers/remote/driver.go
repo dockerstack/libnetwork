b3dcb1fc398e6102fe06774abe9748ca6525e15a
Madhu Venugopal <madhu@docker.com>
2015-05-08 07:01:08 -0700

    Remote Driver registration with Docker Plugins system
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- drivers/remote/driver.go
+++ drivers/remote/driver.go
@@ -3,6 +3,8 @@ package remote
 import (
 	"errors"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/plugins"
 	"github.com/docker/libnetwork/driverapi"
 	"github.com/docker/libnetwork/sandbox"
 	"github.com/docker/libnetwork/types"
@@ -14,19 +16,34 @@ const networkType = "remote"
 
 type driver struct {
 	callback driverapi.DriverCallback
+	endpoint *plugins.Client
 }
 
 // New instance of remote driver returned to LibNetwork
 func New(dc driverapi.DriverCallback) (string, driverapi.Driver) {
 	d := &driver{}
 	d.callback = dc
+	plugins.Handle("Network-Driver", func(name string, client *plugins.Client) {
+		logrus.Infof("New Network-Driver %s (%v) registered", name, client)
+		// Handhake happens here with the Plugin and networkType managed by that plugin is determined.
+
+		var returnData interface{}
+		err := client.Call("handshake/foo/bar", "{foo:bar}", &returnData)
+		if err != nil {
+			logrus.Infof("Remote Network Driver handshake failed")
+			return
+		}
+
+		var networkType string // Parse return_data to get the handshaked networkType
+		d.registerRemoteDriver(networkType, client)
+	})
 	return networkType, d
 }
 
 // Internal Convenience method to register a remote driver.
 // The implementation of this method will change based on the dynamic driver registration design
-func (d *driver) registerRemoteDriver(networkType string) (driverapi.Driver, error) {
-	newDriver := &driver{}
+func (d *driver) registerRemoteDriver(networkType string, client *plugins.Client) (driverapi.Driver, error) {
+	newDriver := &driver{endpoint: client}
 	if d.callback == nil {
 		return nil, errNoCallback
 	}
